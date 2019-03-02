aa022dd8e268f33f05b77ca2f599f640acc4bbeb
zhinki <danielfer@unicauca.edu.co>
2019-03-02 14:50:43 -0500

    front actulizado

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
 
 <!DOCTYPE html>
 <html lang="en">
@@ -8,44 +6,42 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
-    
-    <link rel="stylesheet" href="css/core-style.css">
+         <link rel="stylesheet" href="css/core-style.css">
     <link rel="stylesheet" href="style.css">
 </head>
-<nav class="navbar navbar-default navbar-fixed-top">
-  <div class="container">
-    ...
-  </div>
-</nav>
-
-
-	<body>
-		<nav class="navbar navbar-expand-md  navbar-dark bg-dark">
+<body>
     
+ <nav class="navbar fixed-top navbar-dark bg-dark navbar-expand-md bg-faded justify-content-center">
     
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item active">
-                <a class="navbar-brand  abs" href="" data-target="#myModal" data-toggle="modal">Iniciar sesion</a>
+   
+    <div class="navbar-collapse collapse w-100" id="collapsingNavbar3">
+        <ul class="navbar-nav w-100 ">
+            <li class="nav-item active">
+                <a class="nav-link" href="/">Inicio</a>
             </li>
             <li class="nav-item active">
-                <a class="navbar-brand  abs" href="" data-target="#myModal" data-toggle="modal">Registrar</a>
+                <a class="nav-link" href="">Tienda</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="">Pago</a>
+            </li>
+        </ul>
+        <a href="/" class="navbar-brand d-flex  justify-content-center">Navbar 3</a>
+        <ul class="nav navbar-nav ml-auto w-100 justify-content-end">
+           
+            <li class="nav-item active">
+                <a class="nav-link  " href="/">Iniciar Sesion</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link   " href="">Registrarse</a>
+            </li>
+             <li class="nav-item ">
+                <a href="cart.html" class="cart-nav"><img src="img/core-img/cart2.png" alt=""><a class="text-white">(0)</a></a>
             </li>
         </ul>
     </div>
 </nav>
-<div class="container">
 
-      <!-- Main component for a primary marketing message or call to action -->
-      <div class="jumbotron">
-        <h1>Navbar example</h1>
-        <p>This example is a quick exercise to illustrate how the default, static and fixed to top navbar work. It includes the responsive CSS and HTML, so it also adapts to your viewport and device.</p>
-        <p>To see the difference between static and fixed top navbars, just scroll.</p>
-        <p>
-          <a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">View navbar docs »</a>
-        </p>
-      </div>
-
-    </div>
 
 
     <!-- Search Wrapper Area Start -->
@@ -65,33 +61,16 @@
             <div class="amado-navbar-toggler">
                 <span></span><span></span><span></span>
             </div>
-        </div>
+    </div>
      
-        <!-- Header Area Start -->
-        <header class="header-area clearfix">
-            <!-- Close Icon -->
-                   <!-- Amado Nav -->
-            <nav class="amado-nav">
-                <ul>
-                    <li class="active"><a href="index.html">Inicio</a></li>
-                    <li><a href="shop.html">Tienda</a></li>
-                    <li><a href="product-details.html">Pago</a></li>
-                 </ul>
-            </nav>
-            <!-- Button Group -->
-           
-            <!-- Cart Menu -->
-            <div class="cart-fav-search mb-100">
-                <a href="cart.html" class="cart-nav"><img src="img/core-img/cart.png" alt=""> Cart <span>(0)</span></a>
-                <a href="#" class="fav-nav"><img src="img/core-img/favorites.png" alt=""> Favourite</a>
-                <a href="#" class="search-nav"><img src="img/core-img/search.png" alt=""> Search</a>
-            </div>
-         
-        </header>
-        <!-- Header Area End -->
-
+     <p>salto</p>
+       
+        
         <!-- Product Catagories Area Start -->
-        <div class="products-catagories-area clearfix">
+        <div class="container-fluid">
+           <div class="row">
+            <div class="col col-xl-12">
+        
             <div class="amado-pro-catagory clearfix">
 
                 <!-- Single Catagory -->
@@ -211,8 +190,10 @@
                     </a>
                 </div>
             </div>
-        </div>
+       
         <!-- Product Catagories Area End -->
+          </div>
+        </div>
     </div>
    
 
