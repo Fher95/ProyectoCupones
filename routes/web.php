2361f899c6861fde27cc0374ab4c4c9643b74dfb
bdaniel <bdaniel@unicauca.edu.co>
2019-03-26 22:42:52 -0500

    las rutas se agruparon por rol

--- routes/web.php
+++ routes/web.php
@@ -12,32 +12,38 @@ use App\Cupon;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+
+Auth::routes();
+
 Route::get('/', function () {
     $cupones = Cupon::where('totalAutorizados','>','0')->get();
     return view('index',['cupones'=>$cupones]);
 });
-
-Auth::routes();
-
 Route::get('/index', 'CuponController@show')->name('index');
-Route::get('/home', 'HomeController@home')->name('home');
-
 Route::get('/tienda', 'HomeController@tienda')->name('tienda');
-Route::get('/crearCupon', 'CuponController@create')->name('crearCupon');
-Route::get('/crearAliado', 'AliadoController@create')->name('crearAliado');
-Route::post('/guardarAliado', 'AliadoController@store')->name('storeAliado');
-Route::get('/misCupones', 'CompraController@show')->name('showMisCupones');
-Route::post('/guardarCupon', 'CuponController@store')->name('storeCupon');
-Route::get('/logout', '\App\Http\Controllers\Auth\LoginController@logout');
 
-Route::get('/redimirCupon/{idCupon}/{fechaCompra}', 'RedimidoController@store')->name('storeRedimido');
-Route::post('/comprarCupones/{idCupon}', 'CompraController@store')->name('storeCompra');
-
-Route::get('/visCupon/{idCupon}', [
-    'as' => 'visCupon',
-    'uses' => 'CuponController@showOne',
-]);
-Route::get('/cuponComprado',  function () {
-    return view('cuponComprado'->with('status', '¡Compra Realizada!'));
+Route::group(['middleware' => 'auth'], function(){
+    Route::get('/home', 'HomeController@home')->name('home');
+    Route::get('/logout', '\App\Http\Controllers\Auth\LoginController@logout');
+    Route::post('/comprarCupones/{idCupon}', 'CompraController@store')->name('storeCompra');
+    Route::get('/visCupon/{idCupon}', [
+        'as' => 'visCupon',
+        'uses' => 'CuponController@showOne',
+    ]);
+    Route::get('/cuponComprado',  function () {
+        return view('cuponComprado'->with('status', '¡Compra Realizada!'));
+    });
+    Route::get('/misCupones', 'CompraController@show')->name('showMisCupones');
+    Route::get('/redimirCupon/{idCupon}/{fechaCompra}', 'RedimidoController@store')->name('storeRedimido');
 });
 
+Route::group(['middleware' => ['usuarioAdmin','usuarioPublicista']], function(){
+    Route::get('/crearCupon', 'CuponController@create')->name('crearCupon');
+    Route::post('/guardarCupon', 'CuponController@store')->name('storeCupon');
+});
+
+Route::group(['middleware' => 'usuarioAdmin'], function(){
+    Route::get('/crearAliado', 'AliadoController@create')->name('crearAliado');
+    Route::post('/guardarAliado', 'AliadoController@store')->name('storeAliado');
+});
